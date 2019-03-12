24109694cb2ca3a3f30a728e8aa736da6c7c4353
freya <frybrdrck@gmail.com>
2019-03-12 01:18:18 +0000

    Screen still updates without inputs

--- src/hydra/application.cr
+++ src/hydra/application.cr
@@ -63,10 +63,15 @@ module Hydra
     end
 
     private def handle_keypress(keypress : Keypress | Nil)
-      return unless keypress
-      event = Event.new(keypress)
-      @event_hub.broadcast(event, @state, @elements)
-      update_screen
+      if keypress.nil?
+        # Even if no button is pressed, we should still update the screen
+        update_screen
+        return
+      else
+        event = Event.new(keypress)
+        @event_hub.broadcast(event, @state, @elements)
+        update_screen
+      end
     end
 
     def teardown
