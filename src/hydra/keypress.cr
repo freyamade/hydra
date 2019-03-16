a8891fedc08ddb26efdfdb10dc7ab3e5ef6169db
freya <frybrdrck@gmail.com>
2019-03-16 16:46:35 +0000

    Expanded the character map in keypress.cr

--- src/hydra/keypress.cr
+++ src/hydra/keypress.cr
@@ -1,15 +1,21 @@
 module Hydra
   class Keypress
     MAPPING = {
-      1 => "ctrl-a",
-      3 => "ctrl-c",
-      5 => "ctrl-e",
-      13 => "enter",
-      24 => "ctrl-x",
-      27 => "escape",
-      127 => "backspace",
-      338 => "keypress.page_down",
-      339 => "keypress.page_up",
+          1 => "ctrl-a",
+          3 => "ctrl-c",
+          5 => "ctrl-e",
+         11 => "ctrl-k",
+         13 => "enter",
+         19 => "ctrl-s",
+         23 => "ctrl-w",
+         24 => "ctrl-x",
+         27 => "escape",
+        127 => "backspace",
+        338 => "keypress.page_down",
+        339 => "keypress.page_up",
+      65515 => "left",
+      65514 => "right",
+      65522 => "delete",
     }
 
     getter :name
@@ -29,7 +35,7 @@ module Hydra
       @ctrl_pressed = (@name =~ /^ctrl-/ ? true : false)
     end
 
-    def ctrl_pressed?() : Bool
+    def ctrl_pressed? : Bool
       @ctrl_pressed
     end
   end
