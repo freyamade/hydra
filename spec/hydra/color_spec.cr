5011b9327781ebbeb999e1d666350b5d8bdd8faa
Broderick <cb@cix.ie>
2019-03-21 13:33:01 +0000

    Furthered testing of my 256 colour implementation

--- spec/hydra/color_spec.cr
+++ spec/hydra/color_spec.cr
@@ -9,4 +9,14 @@ describe Hydra::Color do
     # #388697
     Hydra::Color.new(3704471).index.should eq 66
   end
+
+  it "has the correct number of colours in the hash" do
+    Hydra::Color::COLORS.size.should eq 256
+  end
+
+  it "has correct names for each of the hexcodes" do
+    Hydra::Color::HEXES.each do |_, name|
+      Hydra::Color::COLORS[name]?.should_not be_nil
+    end
+  end
 end
